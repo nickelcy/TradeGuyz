c5984a97b77365e7022c007b53f4bb1a26bc5ec4
Genicks <genicks.html@gmail.com>
2025-06-09 12:13:07 -0400

    Update report api

--- reports/main.py
+++ reports/main.py
@@ -44,10 +44,10 @@ if __name__ == '__main__':
     try:
         if devMode:
             print("Running development server.")
-            app.run(host='0.0.0.0', port=50100, debug=True)
+            app.run(host='0.0.0.0', port=5000, debug=True)
         else:
             print("Running production server.")   
-            serve(app, host='0.0.0.0', port=50100, threads=4)
+            serve(app, host='0.0.0.0', port=5000, threads=4)
     except Exception as e: 
         print("There was an error:", e)
 
